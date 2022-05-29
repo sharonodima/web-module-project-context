66f6c6748a376059e00f9ac1f78e04cd7e2a4340
Sharon Odima <odimasharon@gmail.com>
2022-05-29 14:11:48 -0400

    Sharon Odima Completed Unit3.1Module4 Project.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/ProductContext';
+import CartContext from "./contexts/CartContext";
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,21 +16,24 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])		
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+			<div className="App">
+				<CartContext.Provider value ={cart}>
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<ProductContext.Provider value = {{products, addItem}}>
+						<Route exact path="/">
+							<Products />
+						</Route>
+					</ProductContext.Provider>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</div>
 	);
 }
 
